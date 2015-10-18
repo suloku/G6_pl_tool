25ff27bc92395f91f04999b951ab147766280780
suloku <sulokutdcmago@gmail.com>
2015-10-18 11:04:26 +0200

    version 0.1 commit

---
+++ XYORAS Pokemon Link Tool/MainForm.Designer.cs
@@ -0,0 +1,117 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: suloku
+ * Date: 18/10/2015
+ * Time: 9:17
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+namespace XYORAS_Pokemon_Link_Tool
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// Designer variable used to keep track of non-visual components.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+		private System.Windows.Forms.Button loadsave;
+		private System.Windows.Forms.TextBox savegamename;
+		private System.Windows.Forms.Button dump_but;
+		private System.Windows.Forms.Button inject_but;
+		private System.Windows.Forms.TextBox currgame;
+		
+		/// <summary>
+		/// Disposes resources used by the form.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing) {
+				if (components != null) {
+					components.Dispose();
+				}
+			}
+			base.Dispose(disposing);
+		}
+		
+		/// <summary>
+		/// This method is required for Windows Forms designer support.
+		/// Do not change the method contents inside the source code editor. The Forms designer might
+		/// not be able to load this method if it was changed manually.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.loadsave = new System.Windows.Forms.Button();
+			this.savegamename = new System.Windows.Forms.TextBox();
+			this.dump_but = new System.Windows.Forms.Button();
+			this.inject_but = new System.Windows.Forms.Button();
+			this.currgame = new System.Windows.Forms.TextBox();
+			this.SuspendLayout();
+			// 
+			// loadsave
+			// 
+			this.loadsave.Location = new System.Drawing.Point(12, 12);
+			this.loadsave.Name = "loadsave";
+			this.loadsave.Size = new System.Drawing.Size(162, 23);
+			this.loadsave.TabIndex = 0;
+			this.loadsave.Text = "Load XY/ORAS Savegame";
+			this.loadsave.UseVisualStyleBackColor = true;
+			this.loadsave.Click += new System.EventHandler(this.Button1Click);
+			// 
+			// savegamename
+			// 
+			this.savegamename.Location = new System.Drawing.Point(12, 41);
+			this.savegamename.Name = "savegamename";
+			this.savegamename.Size = new System.Drawing.Size(330, 20);
+			this.savegamename.TabIndex = 1;
+			this.savegamename.TextChanged += new System.EventHandler(this.SavegamenameTextChanged);
+			// 
+			// dump_but
+			// 
+			this.dump_but.Enabled = false;
+			this.dump_but.Location = new System.Drawing.Point(12, 67);
+			this.dump_but.Name = "dump_but";
+			this.dump_but.Size = new System.Drawing.Size(162, 23);
+			this.dump_but.TabIndex = 2;
+			this.dump_but.Text = "Dump Pokémon Link Data";
+			this.dump_but.UseVisualStyleBackColor = true;
+			this.dump_but.Click += new System.EventHandler(this.Dump_butClick);
+			// 
+			// inject_but
+			// 
+			this.inject_but.Enabled = false;
+			this.inject_but.Location = new System.Drawing.Point(180, 67);
+			this.inject_but.Name = "inject_but";
+			this.inject_but.Size = new System.Drawing.Size(162, 23);
+			this.inject_but.TabIndex = 4;
+			this.inject_but.Text = "Inject Pokémon Link Data";
+			this.inject_but.UseVisualStyleBackColor = true;
+			this.inject_but.Click += new System.EventHandler(this.Inject_butClick);
+			// 
+			// currgame
+			// 
+			this.currgame.Location = new System.Drawing.Point(180, 14);
+			this.currgame.Name = "currgame";
+			this.currgame.ReadOnly = true;
+			this.currgame.Size = new System.Drawing.Size(73, 20);
+			this.currgame.TabIndex = 5;
+			this.currgame.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(354, 95);
+			this.Controls.Add(this.currgame);
+			this.Controls.Add(this.savegamename);
+			this.Controls.Add(this.dump_but);
+			this.Controls.Add(this.loadsave);
+			this.Controls.Add(this.inject_but);
+			this.Name = "MainForm";
+			this.Text = "XYORAS Pokemon Link Tool 0.1 by suloku";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+	}
+}
---
+++ XYORAS Pokemon Link Tool/MainForm.cs
@@ -0,0 +1,244 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: suloku
+ * Date: 18/10/2015
+ * Time: 9:17
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace XYORAS_Pokemon_Link_Tool
+{
+	/// <summary>
+	/// Description of MainForm.
+	/// </summary>
+	public partial class MainForm : Form
+	{
+		public MainForm()
+		{
+			//
+			// The InitializeComponent() call is required for Windows Forms designer support.
+			//
+			InitializeComponent();
+			
+			//
+			// TODO: Add constructor code after the InitializeComponent() call.
+			//
+		}
+		int game = 0; //1 XY, 2 ORAS
+		string linkfile;
+		byte[] savebuffer_XY = new byte[0x65600];
+		byte[] savebuffer_ORAS = new byte[0x76000];
+		byte[] linkbuffer = new byte[2631];
+
+		//adapted from Gocario's PHBank (www.github.com/gocario/phbank)
+		byte[] ccitt16(byte[] data)
+		// --------------------------------------------------
+		{
+			int len = data.Length;
+			UInt16 crc = 0xFFFF;
+		
+			for (UInt32 i = 0; i < len; i++)
+			{
+				crc ^= ((UInt16)((data[i] << 8)&0x0000FFFF));
+		
+				for (UInt32 j = 0; j < 0x8; j++)
+				{
+					if ((crc & 0x8000) > 0)
+						crc = (UInt16)(((UInt16)((crc << 1)&0x0000FFFF ) ^ 0x1021) &0x0000FFFF);
+					else
+						crc <<= 1;
+				}
+			}
+		
+			return BitConverter.GetBytes(crc);
+		}
+
+		/// <summary>
+		/// Reads data into a complete array, throwing an EndOfStreamException
+		/// if the stream runs out of data first, or if an IOException
+		/// naturally occurs.
+		/// </summary>
+		/// <param name="stream">The stream to read data from</param>
+		/// <param name="data">The array to read bytes into. The array
+		/// will be completely filled from the stream, so an appropriate
+		/// size must be given.</param>
+		public static void ReadWholeArray (Stream stream, byte[] data)
+		{
+		    int offset=0;
+		    int remaining = data.Length;
+		    while (remaining > 0)
+		    {
+		        int read = stream.Read(data, offset, remaining);
+		        if (read <= 0)
+		            throw new EndOfStreamException 
+		                (String.Format("End of stream reached with {0} bytes left to read", remaining));
+		        remaining -= read;
+		        offset += read;
+		    }
+		}
+		private void PDR_read_data()
+		{
+	            System.IO.FileStream saveFile;
+	            saveFile = new FileStream(savegamename.Text, FileMode.Open);
+	            if (saveFile.Length != 0x65600 && saveFile.Length != 0x76000 ){
+	            	savegamename.Text = "";
+	            	MessageBox.Show("Invalid file length", "Error");
+	            	return;
+	            }
+	            if (saveFile.Length == 0x65600){
+	            	game = 1;
+	            	currgame.Text = "X/Y";
+		            ReadWholeArray(saveFile, savebuffer_XY);
+		            saveFile.Close();
+	            }else if (saveFile.Length == 0x76000){
+	            	game = 2;
+	            	currgame.Text = "OR/AS";
+		            ReadWholeArray(saveFile, savebuffer_ORAS);
+		            saveFile.Close();
+	            }
+		}
+		private void PDR_get_data()
+        {
+            OpenFileDialog openFD = new OpenFileDialog();
+            //openFD.InitialDirectory = "c:\\";
+            openFD.Filter = "VI gen save data|main|All Files (*.*)|*.*";
+            if (openFD.ShowDialog() == DialogResult.OK)
+            {
+                #region filename
+                savegamename.Text = openFD.FileName;
+                #endregion
+                PDR_read_data();
+            }
+            
+        }
+		private void PDR_save_data()
+		{	if (savegamename.Text.Length < 1) return;
+            SaveFileDialog saveFD = new SaveFileDialog();
+            //saveFD.InitialDirectory = "c:\\";
+            saveFD.Filter = "VI gen save data|main|All Files (*.*)|*.*";
+            if (saveFD.ShowDialog() == DialogResult.OK)
+            {
+	            System.IO.FileStream saveFile;
+	            saveFile = new FileStream(saveFD.FileName, FileMode.Create);            
+	            //Write file
+	            if (game == 1){
+	            	saveFile.Write(savebuffer_XY, 0, savebuffer_XY.Length);
+	            }else if (game == 2){
+	            	saveFile.Write(savebuffer_ORAS, 0, savebuffer_ORAS.Length);
+	            }
+	            saveFile.Close();
+	            MessageBox.Show("File Saved.", "Save file");
+            }
+		}
+		private void PDR_dump_forest_data()
+		{	if (savegamename.Text.Length < 1) return;
+            SaveFileDialog saveFD = new SaveFileDialog();
+            //saveFD.InitialDirectory = "c:\\";
+            saveFD.Filter = "Pokémon Link Data|*.bin|All Files (*.*)|*.*";
+            if (saveFD.ShowDialog() == DialogResult.OK)
+            {
+	            System.IO.FileStream saveFile;
+	            saveFile = new FileStream(saveFD.FileName, FileMode.Create);            
+	            //Write file
+	            if (game == 1){
+	            	saveFile.Write(savebuffer_XY, 0x1FFFF, 0xA47);
+	            }else if (game == 2) {
+	            	saveFile.Write(savebuffer_ORAS, 0x20FFF, 0xA47);
+	            }
+	            
+	            saveFile.Close();
+	            MessageBox.Show("Pokémon Link data dumped to:\r"+saveFD.FileName+".", "Dump Entralink Forest Data");
+            }
+		}
+		private void PDR_read_forest_data()
+		{
+	            System.IO.FileStream saveFile;
+	            saveFile = new FileStream(linkfile, FileMode.Open);
+	            if (saveFile.Length != 0xA47){
+	            	//linkfile = "";
+	            	MessageBox.Show("Invalid file length", "Error");
+	            	return;
+	            }
+	            ReadWholeArray(saveFile, linkbuffer);
+	            saveFile.Close();
+	            PDR_injectNsave();
+		}
+		private void PDR_get_forest_data()
+        {
+            OpenFileDialog openFD = new OpenFileDialog();
+            //openFD.InitialDirectory = "c:\\";
+            openFD.Filter = "Pokémon Link Data|*.bin|All Files (*.*)|*.*";
+            if (openFD.ShowDialog() == DialogResult.OK)
+            {
+                #region filename
+                linkfile = openFD.FileName;
+                #endregion
+                PDR_read_forest_data();
+            }
+            
+        }
+		private void PDR_injectNsave()
+		{
+
+			if (game == 1){
+				//Put link data in save
+				Array.Copy(linkbuffer, 0, savebuffer_XY, 0x1FFFF, 0xA47);
+				//Get full block for checksum calculation
+				byte[] giftblockbuffer = new byte[0xC48];
+				Array.Copy(savebuffer_XY, 0x1FE00, giftblockbuffer, 0, 0xC48);
+				byte[] tablecrcsum = new byte[2];
+				tablecrcsum = ccitt16(giftblockbuffer);
+				//Put new checksum in savefile
+				Array.Copy(tablecrcsum, 0, savebuffer_XY, 0x6559A, 2);
+			}
+			else if (game == 2){
+				//Put link data in save
+				Array.Copy(linkbuffer, 0, savebuffer_ORAS, 0x20FFF, 0xA47);
+				//Get full block for checksum calculation
+				byte[] giftblockbuffer = new byte[0xC48];
+				Array.Copy(savebuffer_ORAS, 0x20E00, giftblockbuffer, 0, 0xC48);
+				byte[] tablecrcsum = new byte[2];
+				tablecrcsum = ccitt16(giftblockbuffer);
+				//Put new checksum in savefile
+				Array.Copy(tablecrcsum, 0, savebuffer_ORAS, 0x75F9A, 2);
+			}
+			//Write Data
+			PDR_save_data();
+		}
+		void Button1Click(object sender, EventArgs e)
+		{
+			PDR_get_data();
+		}
+		void Dump_butClick(object sender, EventArgs e)
+		{
+			if (savegamename.Text.Length < 1) return;
+			PDR_dump_forest_data();
+		}
+		void SavegamenameTextChanged(object sender, EventArgs e)
+		{
+			if (savegamename.Text.Length > 0){
+				dump_but.Enabled = true;
+				inject_but.Enabled = true;
+			}else{
+				dump_but.Enabled = false;
+				inject_but.Enabled = false;
+			}
+
+		}
+		void Inject_butClick(object sender, EventArgs e)
+		{
+			PDR_get_forest_data();
+		}
+
+	}
+}
